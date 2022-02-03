d62113dd79b1a8d9210c15af39731c8cd50d67a1
ryancarloscorrea <ryancarlos38@gmail.com>
2022-02-03 06:22:07 -0400

    feat: added inital configs, Header component

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import { AppProps } from 'next/app'
+import '../styles/global.scss'
+import {Header} from "../components/Header";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <>
+    <Header/>
+    <Component {...pageProps} />
+  </>
+}
+
+export default MyApp
