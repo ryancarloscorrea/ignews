06c4757ff9d2c6efdcedb4268962b075c7438de4
ryancarloscorrea <ryancarlos38@gmail.com>
2022-08-15 19:49:41 -0400

    feat: added styles

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,17 @@
+import React, {FC} from 'react';
+import styles from './styles.module.scss'
+
+interface SubscribeButtonProps {
+    priceId: string
+}
+
+export const SubscribeButton:FC<SubscribeButtonProps> = ({priceId}) => {
+    return (
+        <button
+        type="button"
+        className={styles.subscribeButton}>
+            Subscribe now
+        </button>
+    );
+};
+
